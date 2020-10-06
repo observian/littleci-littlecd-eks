7fb57b007a6106e6b8c95ef10e35e0c8fedd2caa
dan <dan.taylor@observian.com>
2020-10-05 18:20:46 -0600

    adding some output and changing layering on Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
 FROM ubuntu:18.04
 ENV TZ=America/Denver
-RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone && \
-    apt-get update && apt-get install -y apt-transport-https gnupg2 curl && \
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+RUN apt-get update && apt-get install -y apt-transport-https gnupg2 curl && \
     curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - && \
     echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list && \
     apt-get update -y && \
     apt-get install -y kubectl awscli gettext
+WORKDIR /app
 COPY entrypoint.sh .
-RUN chmod +x entrypoint.sh
-ENTRYPOINT [ "entrypoint.sh" ]
+RUN chmod +x ./entrypoint.sh
+ENTRYPOINT [ "/app/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,37 +2,53 @@
 set -e
 
 function main() {
+    echo "validating inputs"
     validate "${INPUT_ACCESS_KEY_ID}" "access_key_id"
     validate "${INPUT_SECRET_ACCESS_KEY}" "secret_access_key"
     validate "${INPUT_REGION}" "region"
     validate "${INPUT_ACCOUNT_ID}" "account_id"
     validate "${INPUT_REPO}" "repo"
-    validate "${INPUT_CLUSTER_NAME}" "eks_cluster_name"
+    validate "${INPUT_EKS_CLUSTER_NAME}" "eks_cluster_name"
 
+    echo "inputs are valid"
+    echo "setting up env variables for the build"
     export AWS_ACCESS_KEY_ID=$INPUT_ACCESS_KEY_ID
     export AWS_SECRET_ACCESS_KEY=$INPUT_SECRET_ACCESS_KEY
     export AWS_DEFAULT_REGION=$INPUT_REGION
+    echo "done"
 
+    
+    echo "logging in to ECR and EKS"
     ecr get-login-password --region $INPUT_REGION  | docker login $INPUT_ACCOUNT_ID.dkr.ecr.$INPUT_REGION.amazonaws.com
-    aws eks update-kubeconfig --name $INPUT_CLUSTER_NAME --region $INPUT_REGION
-
+    aws eks update-kubeconfig --name $INPUT_EKS_CLUSTER_NAME --region $INPUT_REGION
+    echo "done"
+    
     local TAG=$INPUT_TAGS
     local tag_args=""
     local TAG_ARGS=$(echo "$TAG" | tr "," "\n")
+
     ACCOUNT_URL="$INPUT_ACCOUNT_ID.dkr.ecr.$INPUT_REGION.amazonaws.com"
+    echo "setting up docker tags"
     for tag in $TAG_ARGS; do
         #tag many times, but only do the build once
         tag_args="$tag_args -t $ACCOUNT_URL/$INPUT_REPO:$tag"
+
     done
+    echo "done"
     docker build -f $INPUT_DOCKERFILE $tag_args $INPUT_PATH
     #push up each tag
+    echo "pushing up all tags"
     for tag in $TAG_ARGS; do
         docker push $ACCOUNT_URL/$INPUT_REPO:$tag
     done
-
+    echo "done"
     export IMAGE_TAG=$INPUT_K8S_IMAGE_TAG
+    echo "substituting image name"
     envsubst < $INPUT_K8S_MANIFEST > deployment.yml
+    echo "done"
+    echo "applying deployment to $INPUT_EKS_CLUSTER_NAME"
     kubectl apply -f deployment.yml
+
    
 }
 
@@ -42,4 +58,4 @@ function validate() {
         exit 1
     fi
 }
-
+main
