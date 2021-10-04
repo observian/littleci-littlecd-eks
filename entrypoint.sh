aa7a81222430590aefbfcd9ffab5c13278bdb0bc
Steven Beard <steven.beard@observian.com>
2021-10-04 12:21:17 -0400

    use envsubst to fix tag issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -57,9 +57,9 @@ function main() {
     export IMAGE_TAG=$INPUT_K8S_IMAGE_TAG
     echo "substituting image name"
     echo "done"
-    sed -i "s/'${IMAGE_TAG}'/$tag/" ${INPUT_OVERLAY_PATH}/deployment
     echo "applying deployment to $INPUT_EKS_CLUSTER_NAME"
-
+    envsubst < ${INPUT_OVERLAY_PATH}/deployment.yml tmp.yml
+    mv tmp.yml ${INPUT_OVERLAY_PATH}/deployment.yml
     kustomize build $INPUT_OVERLAY_PATH | kubectl apply -f -
 }
 
