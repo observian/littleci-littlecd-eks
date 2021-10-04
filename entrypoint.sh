e91466137cf6707480656d1dad76b9739b102ad9
Steven Beard <steven.beard@observian.com>
2021-10-04 10:37:58 -0400

    add sed command to update image tag before build

--- entrypoint.sh
+++ entrypoint.sh
@@ -57,9 +57,9 @@ function main() {
     export IMAGE_TAG=$INPUT_K8S_IMAGE_TAG
     echo "substituting image name"
     echo "done"
+    sed -i "s/'${IMAGE_TAG}'/$tag" ${INPUT_OVERLAY_PATH}/deployment
     echo "applying deployment to $INPUT_EKS_CLUSTER_NAME"
-    echo "Before kustomize" $(pwd)
-    ls
+
     kustomize build $INPUT_OVERLAY_PATH | kubectl apply -f -
 }
 
